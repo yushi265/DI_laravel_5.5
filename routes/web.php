d10a48bfa11e6cbaaff5cbb4c494b8ae69585490
yushi265 <ae265.1108@gmail.com>
2021-01-17 08:57:49 +0900

    もういっかいつくる

--- routes/web.php
+++ routes/web.php
@@ -10,22 +10,3 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', 'PostsController@index');
-
-// Route::get('/posts/{id}', 'PostsController@show');
-Route::get('/posts/{post}', 'PostsController@show')->where('post', '[0-9]+');
-
-Route::get('/posts/create', 'PostsController@create');
-
-Route::post('/posts', 'PostsController@store');
-
-Route::get('/posts/{post}/edit','PostsController@edit');
-
-Route::patch('/posts/{post}', 'PostsController@update');
-
-Route::delete('/posts/{post}', 'PostsController@destroy');
-
-Route::post('/posts/{post}/comments', 'CommentsController@store');
-
-Route::delete('/posts/{post}/comment/{comment}', 'CommentsController@destroy');
